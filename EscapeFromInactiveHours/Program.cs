ab2d9d1f476652527f2454733bc44e24690387ba
pedro7 <idactgithub8@maild.org>
2021-04-05 23:32:32 +0100

    Fix logic- was inverted

--- EscapeFromInactiveHours/Program.cs
+++ EscapeFromInactiveHours/Program.cs
@@ -14,8 +14,9 @@ namespace EscapeFromInactiveHours
 
             System.Timers.Timer myTimer = new System.Timers.Timer();
             myTimer.Elapsed += MyTimer_Elapsed;
-            myTimer.Interval = 2000;
+            myTimer.Interval = TimeSpan.FromHours(4).TotalMilliseconds;
             myTimer.AutoReset = true;
+            MyTimer_Elapsed(null, null);
             myTimer.Start();
             
             while (true) {
@@ -26,22 +27,23 @@ namespace EscapeFromInactiveHours
         private static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
             var now = DateTime.Now;
-            int inicio = now.AddHours(-9).AddMinutes(-now.Minute).AddSeconds(-now.Second).Hour;
-            int fim = now.AddHours(-1).AddMinutes(-now.Minute).AddSeconds(-now.Second).Hour;
+            int inicio = now.AddHours(-1).AddMinutes(-now.Minute).AddSeconds(-now.Second).Hour;
+            int fim = now.AddHours(8).AddMinutes(-now.Minute).AddSeconds(-now.Second).Hour;
+            
 
             Console.Out.WriteLine();
-            Console.Out.WriteLine("Setting active hours to: " + inicio + " to " + fim);
+            Console.Out.WriteLine(DateTime.Now + " Setting active hours to: " + inicio + " to " + fim);
 
             using (RegistryKey registryKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey(@"SOFTWARE\Microsoft\WindowsUpdate\UX\Settings", true))
             {
                 //foreach()
 
-                Console.WriteLine(registryKey.SubKeyCount);//registry is not null
-                foreach (var VARIABLE in registryKey.GetValueNames())
-                {
-                    Console.WriteLine(VARIABLE);//here I can see I have many keys
-                                                //no need to switch to x64 as suggested on other posts
-                }
+                //Console.WriteLine(registryKey.SubKeyCount);//registry is not null
+                //foreach (var VARIABLE in registryKey.GetValueNames())
+                //{
+                //    Console.WriteLine(VARIABLE);//here I can see I have many keys
+                //                                //no need to switch to x64 as suggested on other posts
+                //}
 
                 registryKey.SetValue("ActiveHoursStart", inicio, RegistryValueKind.DWord);
                 registryKey.SetValue("ActiveHoursEnd", fim, RegistryValueKind.DWord);
